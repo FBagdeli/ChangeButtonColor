8e4e40f826b245ccd4e8fa2ed79fa5d783d73f5d
FBagdeli <f.bagdeli13@outlook.com>
2023-09-06 23:44:50 +0200

    TextField
    I add a TextField that ask user to put his name
    and after the user click on the button , his name will show up on text

--- app/src/main/java/com/farshadchalenges/changebuttoncolor/MainActivity.kt
+++ app/src/main/java/com/farshadchalenges/changebuttoncolor/MainActivity.kt
@@ -13,11 +13,13 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -65,8 +67,11 @@ fun ChangeColor() {
             val myText = remember {
                 mutableStateOf("Show Your message Here")
             }
-            val myBoolean = remember {
+            val showOrHide = remember {
                 mutableStateOf(true)
+            }
+            val myTextFieldValue = remember {
+                mutableStateOf("")
             }
 
             Column(
@@ -77,9 +82,9 @@ fun ChangeColor() {
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
 
-                if (!myBoolean.value){
+                if (!showOrHide.value) {
                     Text(
-                        text = myText.value,
+                        text = myTextFieldValue.value,
                         fontSize = 24.sp,
                         color = myButtonTextColor.value,
                         fontFamily = FontFamily.SansSerif,
@@ -90,29 +95,24 @@ fun ChangeColor() {
                     )
                 }
 
-
                 Spacer(modifier = Modifier.size(20.dp))
 
                 Button(
                     onClick = {
-
-                        if (myBoolean.value){
+                        if (showOrHide.value) {
                             myButtonBackGroundColor.value = Color.Red
                             myButtonTextColor.value = Color.Yellow
                             myBackGroundColor.value = Color.White
                             myButtonText.value = "You did it"
-                            myText.value = "You Should Finish it!"
-                            myBoolean.value = false
-                        }
-                        else{
+                            showOrHide.value = false
+                        } else {
                             myButtonBackGroundColor.value = Color.Blue
                             myButtonTextColor.value = Color.Black
                             myBackGroundColor.value = Color.DarkGray
                             myButtonText.value = "Do Your Magic"
-                            myText.value = "Show your message Here"
-                            myBoolean.value = true
+                            myTextFieldValue.value = ""
+                            showOrHide.value = true
                         }
-
 
                     },
                     modifier = Modifier.size(250.dp, 100.dp),
@@ -120,7 +120,6 @@ fun ChangeColor() {
                         containerColor = myButtonBackGroundColor.value
                     ),
                     border = BorderStroke(3.dp, Color.DarkGray)
-
                 ) {
                     Text(
                         text = myButtonText.value,
@@ -131,11 +130,25 @@ fun ChangeColor() {
                         textAlign = TextAlign.End
                     )
                 }
+                Spacer(modifier = Modifier.size(20.dp))
+
+                TextField(
+                    value = myTextFieldValue.value,
+                    onValueChange = {
+                        myTextFieldValue.value = it
+                    },
+                    label = {
+                        Text(
+                            text = "Enter Your Name",
+                            color = Color.Blue
+                        )
+                    },
+                    modifier = Modifier.width(300.dp),
+                    maxLines = 4
+                )
             }
         }
     }
-
-
 }
 
 @Preview(showBackground = true)
